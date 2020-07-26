e36b0baf085ed4a6189ccd9c16f7cf2ae8c2b9ba
i-am-a-person78 <67607940+i-am-a-person78@users.noreply.github.com>
2020-07-26 09:06:06 -0700

    some stuff

--- stuff/src/App.js
+++ stuff/src/App.js
@@ -8,15 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Idk why I did this
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Click if you're weird
         </a>
       </header>
     </div>
